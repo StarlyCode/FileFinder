2eb6bf1c2ea8f782aed5be12759c34a630f65bfe
Paul Staszko <paulstaszko@gmail.com>
2024-10-14 14:28:21 -0500

    ok

--- FileFinderTests/RulesTests.fs
+++ FileFinderTests/RulesTests.fs
@@ -6,20 +6,20 @@ open FsUnit
 open Xunit
 //Op: End
 module RulesTests =
-    let ViewRule =
-        {
-            Name = "ASP.NET View HTML"
-            PossibleDirs = [@"C:\Dev\WesternCap\Cricket.Intranet\Views"]
-            Patterns = [
-                @"Views\{Controller}\{Action}.cshtml"
-                @"Views\{Controller}\{Action}_{SubAction}.cshtml"
-                @"Areas\{Area}\Views\{Controller}\{Action}.cshtml"
-                @"Areas\{Area}\Views\{Controller}\{Action}_{SubAction}.cshtml"
-            ]
-        }
-
     [<Fact>]
     let ``ResolvePatterns - x`` () =
+        let viewRule =
+            {
+                Name = "ASP.NET View HTML"
+                PossibleDirs = [@"C:\Dev\WesternCap\Cricket.Intranet\Views"]
+                Patterns = [
+                    @"Views\{Controller}\{Action}.cshtml"
+                    @"Views\{Controller}\{Action}_{SubAction}.cshtml"
+                    @"Areas\{Area}\Views\{Controller}\{Action}.cshtml"
+                    @"Areas\{Area}\Views\{Controller}\{Action}_{SubAction}.cshtml"
+                ]
+            }
+
         let factors = 
             [
                 {
@@ -32,11 +32,46 @@ module RulesTests =
                 }
             ]
 
-        ResolvePatterns ViewRule factors
+        ResolvePatterns viewRule factors
         |> should equal 
             [
                 "Views\Home\Index.cshtml"
                 "Views\Home\Index_{SubAction}.cshtml"
                 "Areas\{Area}\Views\Home\Index.cshtml"
                 "Areas\{Area}\Views\Home\Index_{SubAction}.cshtml"
+            ]
+
+    [<Fact>]
+    let ``ApplyFactorsToRule - x`` () =
+        let viewRule =
+            {
+                Name = "ASP.NET View HTML"
+                PossibleDirs = [@"C:\Dev\WesternCap\Cricket.Intranet\Views"]
+                Patterns = [
+                    @"Views\{Controller}\{Action}.cshtml"
+                    @"Views\{Controller}\{Action}_{SubAction}.cshtml"
+                    @"Areas\{Area}\Views\{Controller}\{Action}.cshtml"
+                    @"Areas\{Area}\Views\{Controller}\{Action}_{SubAction}.cshtml"
+                ]
+            }
+
+        let factors = 
+            [
+                {
+                    PlaceHolder = "Controller"
+                    Value = "Home"
+                }
+                {
+                    PlaceHolder = "Action"
+                    Value = "Index"
+                }
+            ]
+
+        ApplyFactorsToRule viewRule factors
+        |> should equal 
+            [
+                "C:\Dev\WesternCap\Cricket.Intranet\Views\Views\Home\Index.cshtml"
+                "C:\Dev\WesternCap\Cricket.Intranet\Views\Views\Home\Index_{SubAction}.cshtml"
+                "C:\Dev\WesternCap\Cricket.Intranet\Views\Areas\{Area}\Views\Home\Index.cshtml"
+                "C:\Dev\WesternCap\Cricket.Intranet\Views\Areas\{Area}\Views\Home\Index_{SubAction}.cshtml"
             ]
