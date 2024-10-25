779b4ca19edab8c390b0c887655c9d9cd1886dff
Paul Staszko <paulstaszko@gmail.com>
2024-10-25 15:38:25 -0500

    remove winmerge equals

--- FileFinderTests/WinMerge.fs
+++
@@ -1,7 +0,0 @@
-﻿module WinMerge
-    let serialize x = Newtonsoft.Json.JsonConvert.SerializeObject(x, Newtonsoft.Json.Formatting.Indented)
-    let AreEqualWinMerge exp act =
-        let exp = exp |> serialize
-        let act = act |> serialize
-        WinMergeEquals.WinMergeEquals.AreEqualWinMerge exp act WinMergeEquals.WhitespaceSimplify.None "Expected" "Actual"
-        
